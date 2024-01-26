bbfee2de3a2ab6c654d05082107497bbfd076b9a
deepakagashe18 <agashedeepak55@gmail.com>
2024-01-26 15:57:46 +0530

    updated

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const sheet = workbook.Sheets[sheetname];
 
 const jsonData = xlsx.utils.sheet_to_json(sheet);  //converting data to json format
 
-
-
 function excelDateToJSDate(excelDate) {
     return new Date((excelDate - (25567 + 1)) * 86400 * 1000);
 }
@@ -27,30 +25,29 @@ function sameDay(startDate,endDate){
         console.log('Number of entries:', jsonData.length);
 
         // Sort entries based on 'Time' column
-    jsonData.sort((entry1, entry2) => {
+    const sortedData = jsonData.sort((entry1, entry2) => {
         const time1 = excelDateToJSDate(entry1['Time']);
         const time2 = excelDateToJSDate(entry2['Time']);
         return time1 - time2;
     });
+    // let entriesProcessed = 0;
 
-    
-
-
-        for(const entry of jsonData)
+    for (let i = 0; i < sortedData.length - 1; i++) 
         {
             //  console.log('Entry:', entry);
+            const entry = sortedData[i];
             const position = entry['Position ID'];
             const name = entry['Employee Name'];
             const timeIn = excelDateToJSDate(entry['Time']);
             const timeOut = excelDateToJSDate(entry['Time Out']);
-            const timeCard = entry['Timecard Hours (as Time)']
-            const timecardHoursNumeric = parseInt(timeCard.split(':')[0], 10);
-            console.log();
+         
             const workTime = (timeOut - timeIn) / (1000 * 60 * 60);
 
             const payCycleStart = new Date(entry['Pay Cycle Start']);
             const payCycleEnd = new Date(entry['Pay Cycle End']);
-            
+
+            // entriesProcessed++;
+
             let consecutiveDays = 0;
 
             let currentDate = payCycleStart;
@@ -65,7 +62,7 @@ function sameDay(startDate,endDate){
     }
 
 
-            console.log('Consecutive Days Worked:', consecutiveDays);
+            // console.log('Consecutive Days Worked:', consecutiveDays);
 
             if(consecutiveDays >= 7)
             {
@@ -77,14 +74,16 @@ function sameDay(startDate,endDate){
                 console.log(`${position} ${name} - Worked less than 10 hours but greater than an hour.`)
             }
 
-            if(timecardHoursNumeric > 14)
+            if(workTime > 14)
             {
-                console.log(`${position} ${name} -  14 HOURS IN A SINGLE SHIFT.`)
+                console.log(`${position} ${name} -  Worked 14 hours in a single shift.`)
             }
         }
+        // console.log(`Total entries processed: ${entriesProcessed}`);
     }
 
     employeeWork();
+    
 }
 
 const filePath = 'data.xlsx';
